d1bef4fbcd51cd76a4938e044da69c7fa1512183
Analise Burtet <analise.burtet@vtex.com.br>
2020-01-27 17:21:10 -0300

    iniciado desenvolvimento do componente buytogether

---
+++ react/BuyTogether.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyTogether: StorefrontFunctionComponent<any> = () => {
+  return (
+    <div className="testeteste">
+      <h1>algo aqui </h1>
+    </div>
+  )
+}
+
+export default BuyTogether
